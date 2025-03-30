2aa2036c0d64b21281d9f6b3a5ee0d1009a7c421
Sergey Kosarevsky <sk@linderdaum.com>
2025-03-30 03:30:04 -0700

    Fixed wrong dst `VkImageSubresourceRange` in `cmdCopyImage()`

--- lvk/vulkan/VulkanClasses.cpp
+++ lvk/vulkan/VulkanClasses.cpp
@@ -2809,7 +2809,7 @@ void lvk::CommandBuffer::cmdCopyImage(TextureHandle src,
       StageAccess{.stage = VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT, .access = VK_ACCESS_2_MEMORY_READ_BIT | VK_ACCESS_2_MEMORY_WRITE_BIT},
       VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
       newLayout,
-      rangeSrc);
+      rangeDst);
 
   imgDst->vkImageLayout_ = newLayout;
 }
